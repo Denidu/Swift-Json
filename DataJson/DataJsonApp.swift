1988154c551bf12bd805f58a78db13ab4435dbaa
Denidu Gamage <denidugamage21@gmail.com>
2024-10-17 09:21:50 +0530

    Initial Commit

---
+++ DataJson/DataJsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataJsonApp.swift
+//  DataJson
+//
+//  Created by Denidu Gamage on 2024-10-17.
+//
+
+import SwiftUI
+
+@main
+struct DataJsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
